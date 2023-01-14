d64411e43ba968822b4f4e97fcd64b7e687e2f9b
Javier Lopez de Ancos <javier.lopezdeancos@wayflyer.com>
2023-01-15 00:40:37 +0100

    chore: initial commit

---
+++ packages/cuoore-element-button/index.ts
@@ -0,0 +1,50 @@
+import { prefix } from 'cuoore-element'
+
+export class Button extends HTMLElement {
+  label: string
+
+  type?: string
+
+  shadow: ShadowRoot
+
+  template: string
+
+  constructor() {
+    super()
+
+    this.label = this.getAttribute('label') || ''
+    this.type = this.getAttribute('type') || undefined
+    this.shadow = this.attachShadow({ mode: 'open' })
+
+    this.template = `
+      <button
+        type="${this.type}">
+          ${this.label}
+      </button>
+    `
+  }
+
+  connectedCallback(): void {
+    this.render()
+  }
+
+  attributeChangedCallback(attrName: 'label' & 'type', oldVal: string, newVal: string): void {
+    if (attrName === 'label') {
+      this.label = newVal
+    }
+
+    if (attrName === 'type') {
+      this.type = newVal
+    }
+
+    this.render()
+  }
+
+  render() {
+    this.shadow.innerHTML = this.template
+  }
+}
+
+if (!window.customElements.get(`${prefix}-button`)) {
+  window.customElements.define(`${prefix}-button`, Button)
+}
